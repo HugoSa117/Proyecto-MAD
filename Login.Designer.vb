aa43b4ba1dbeab903113fc5b936ea87736bed05c
Hugo Sandoval <hasa2000@hotmail.com>
2021-10-17 20:42:55 -0500

    Primer Commit
    Agregamos el proyecto a GitHub

---
+++ Login.Designer.vb
@@ -0,0 +1,239 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TittleBar = New System.Windows.Forms.Panel()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.txtUser = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPass = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.RadioBTN_Empleado = New System.Windows.Forms.RadioButton()
+        Me.RadioBTN_Cliente = New System.Windows.Forms.RadioButton()
+        Me.TittleBar.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TittleBar
+        '
+        Me.TittleBar.BackColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(38, Byte), Integer), CType(CType(70, Byte), Integer))
+        Me.TittleBar.Controls.Add(Me.PictureBox2)
+        Me.TittleBar.Controls.Add(Me.PictureBox1)
+        Me.TittleBar.Dock = System.Windows.Forms.DockStyle.Top
+        Me.TittleBar.Location = New System.Drawing.Point(0, 0)
+        Me.TittleBar.Name = "TittleBar"
+        Me.TittleBar.Size = New System.Drawing.Size(425, 50)
+        Me.TittleBar.TabIndex = 0
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox2.Image = Global.Proyecto_MAD.My.Resources.Resources.Minimize_Icon
+        Me.PictureBox2.Location = New System.Drawing.Point(371, 16)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(16, 16)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox1.Image = Global.Proyecto_MAD.My.Resources.Resources.Close_Icon
+        Me.PictureBox1.Location = New System.Drawing.Point(394, 16)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(16, 16)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(38, Byte), Integer), CType(CType(70, Byte), Integer))
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel2.Location = New System.Drawing.Point(0, 620)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(425, 15)
+        Me.Panel2.TabIndex = 1
+        '
+        'txtUser
+        '
+        Me.txtUser.BackColor = System.Drawing.Color.FromArgb(CType(CType(80, Byte), Integer), CType(CType(94, Byte), Integer), CType(CType(129, Byte), Integer))
+        Me.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtUser.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtUser.ForeColor = System.Drawing.Color.White
+        Me.txtUser.Location = New System.Drawing.Point(46, 242)
+        Me.txtUser.Name = "txtUser"
+        Me.txtUser.Size = New System.Drawing.Size(350, 20)
+        Me.txtUser.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.Color.Silver
+        Me.Label1.Location = New System.Drawing.Point(46, 218)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(90, 21)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "UserName"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.ForeColor = System.Drawing.Color.Silver
+        Me.Label2.Location = New System.Drawing.Point(46, 287)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(82, 21)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Password"
+        '
+        'txtPass
+        '
+        Me.txtPass.BackColor = System.Drawing.Color.FromArgb(CType(CType(80, Byte), Integer), CType(CType(94, Byte), Integer), CType(CType(129, Byte), Integer))
+        Me.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtPass.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtPass.ForeColor = System.Drawing.Color.White
+        Me.txtPass.Location = New System.Drawing.Point(46, 311)
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.Size = New System.Drawing.Size(350, 20)
+        Me.txtPass.TabIndex = 4
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.CheckBox1.ForeColor = System.Drawing.Color.Silver
+        Me.CheckBox1.Location = New System.Drawing.Point(46, 345)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(110, 21)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "Recuérdame"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(17, Byte), Integer), CType(CType(97, Byte), Integer), CType(CType(238, Byte), Integer))
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(46, 397)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(350, 50)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Login"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.ActiveLinkColor = System.Drawing.Color.FromArgb(CType(CType(17, Byte), Integer), CType(CType(97, Byte), Integer), CType(CType(238, Byte), Integer))
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Font = New System.Drawing.Font("Century Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.LinkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline
+        Me.LinkLabel1.LinkColor = System.Drawing.Color.Silver
+        Me.LinkLabel1.Location = New System.Drawing.Point(125, 525)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(184, 20)
+        Me.LinkLabel1.TabIndex = 8
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "¿Olvidé mi contraseña?"
+        '
+        'RadioBTN_Empleado
+        '
+        Me.RadioBTN_Empleado.AutoSize = True
+        Me.RadioBTN_Empleado.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.RadioBTN_Empleado.ForeColor = System.Drawing.Color.Silver
+        Me.RadioBTN_Empleado.Location = New System.Drawing.Point(46, 188)
+        Me.RadioBTN_Empleado.Name = "RadioBTN_Empleado"
+        Me.RadioBTN_Empleado.Size = New System.Drawing.Size(93, 21)
+        Me.RadioBTN_Empleado.TabIndex = 9
+        Me.RadioBTN_Empleado.TabStop = True
+        Me.RadioBTN_Empleado.Text = "Empleado"
+        Me.RadioBTN_Empleado.UseVisualStyleBackColor = True
+        '
+        'RadioBTN_Cliente
+        '
+        Me.RadioBTN_Cliente.AutoSize = True
+        Me.RadioBTN_Cliente.Font = New System.Drawing.Font("Century Gothic", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.RadioBTN_Cliente.ForeColor = System.Drawing.Color.Silver
+        Me.RadioBTN_Cliente.Location = New System.Drawing.Point(145, 188)
+        Me.RadioBTN_Cliente.Name = "RadioBTN_Cliente"
+        Me.RadioBTN_Cliente.Size = New System.Drawing.Size(72, 21)
+        Me.RadioBTN_Cliente.TabIndex = 10
+        Me.RadioBTN_Cliente.TabStop = True
+        Me.RadioBTN_Cliente.Text = "Cliente"
+        Me.RadioBTN_Cliente.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(49, Byte), Integer), CType(CType(59, Byte), Integer), CType(CType(104, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(425, 635)
+        Me.Controls.Add(Me.RadioBTN_Cliente)
+        Me.Controls.Add(Me.RadioBTN_Empleado)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtPass)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtUser)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.TittleBar)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Opacity = 0.95R
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.TittleBar.ResumeLayout(False)
+        Me.TittleBar.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TittleBar As Panel
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents txtUser As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPass As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents RadioBTN_Empleado As RadioButton
+    Friend WithEvents RadioBTN_Cliente As RadioButton
+End Class
---
+++ Login.vb
@@ -0,0 +1,72 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Login
+#Region "Form Behaviors"
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click 'BOTON CERRAR
+        Application.Exit()
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click 'BOTON MINIMIZAR
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+#End Region
+
+#Region "Drag Form"
+    <DllImport("user32.DLL", EntryPoint:="ReleaseCapture")>
+    Private Shared Sub ReleaseCapture()
+    End Sub
+
+    <DllImport("user32.DLL", EntryPoint:="SendMessage")>
+    Private Shared Sub SendMessage(hWnd As IntPtr, wMsg As Integer, wParam As Integer, lParam As Integer)
+
+    End Sub
+
+
+    Private Sub TittleBar_MouseMove(sender As Object, e As MouseEventArgs) Handles TittleBar.MouseMove 'Mover la pantalla 
+        ReleaseCapture()
+        SendMessage(Me.Handle, &H112&, &HF012&, 0)
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+        ReleaseCapture()
+        SendMessage(Me.Handle, &H112&, &HF012&, 0)
+    End Sub
+
+
+#End Region
+
+#Region "Customize Controls"
+    Private Sub CustomizeComponents()
+        'Texto del username
+        txtUser.AutoSize = False
+        txtUser.Size = New Size(350, 35)
+
+        'texto de password
+        txtPass.AutoSize = False
+        txtPass.Size = New Size(350, 35)
+        txtPass.UseSystemPasswordChar = True
+    End Sub
+
+    Private Sub Button1_Paint(sender As Object, e As PaintEventArgs) Handles Button1.Paint 'ESQUINAS REDONDAS DE BOTON LOGIN
+        Dim buttonPath As Drawing2D.GraphicsPath = New Drawing2D.GraphicsPath()
+        Dim myRectangle As Rectangle = Button1.ClientRectangle
+        myRectangle.Inflate(0, 40)
+        buttonPath.AddEllipse(myRectangle)
+        Button1.Region = New Region(buttonPath)
+    End Sub
+
+#End Region
+
+    Public Sub New()
+
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        CustomizeComponents()
+    End Sub
+
+
+End Class
